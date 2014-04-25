4a5e077a9ada48d75160f52becc73854dc6ab12a
Philip Nelson <me@pnelson.ca>
2014-04-25 08:52:12 -0700

    Document important/tested parts of middleware.

--- middleware/builder.go
+++ middleware/builder.go
@@ -1,16 +1,32 @@
+/*
+Package middleware implements a middleware stack inspired by Rack.
+
+Like Rack, each layer is closed over the others. Unlike Rack, this is not done
+every request. Stack setup is intended to be done before starting the server.
+The outermost layer is cached.
+
+My primary focus here was to stay as close as possible to the http.Handler
+interface provided by the Go standard library. You can see some examples in
+standard.go that just simply wrap some standard library middleware functions
+into middleware that this package can process.
+*/
 package middleware
 
 import (
 	"net/http"
 )
 
+// A Middleware is any function that accepts an http.Handler and returns an
+// http.Handler. The bottom of the stack will be a nil http.Handler.
 type Middleware func(http.Handler) http.Handler
 
+// A Builder builds the middleware stack.
 type Builder struct {
 	handler    http.Handler
 	middleware []Middleware
 }
 
+// Use adds a Middleware function to the stack.
 func (b *Builder) Use(f Middleware) {
 	b.handler = http.Handler(nil)
 	b.middleware = append(b.middleware, f)
@@ -19,12 +35,15 @@ func (b *Builder) Use(f Middleware) {
 	}
 }
 
+// UseBuilder adds an existing middleware stack to this stack.
 func (b *Builder) UseBuilder(builder *Builder) {
 	for _, m := range builder.middleware {
 		b.Use(m)
 	}
 }
 
+// UseHandler adds an http.Handler to the stack. When the stack is being
+// processed, the next http.Handler will be called after the provided handler.
 func (b *Builder) UseHandler(f http.Handler) {
 	b.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -36,10 +55,13 @@ func (b *Builder) UseHandler(f http.Handler) {
 	})
 }
 
+// UseHandlerFunc is a convenience wrapper over UseHandler.
 func (b *Builder) UseHandlerFunc(f func(http.ResponseWriter, *http.Request)) {
 	b.UseHandler(http.HandlerFunc(f))
 }
 
+// ServeHTTP implements the http.Handler interface. It starts off by invoking
+// the outermost layer of the stack.
 func (b *Builder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	b.handler.ServeHTTP(w, r)
 }
--- middleware/standard.go
+++ middleware/standard.go
@@ -5,12 +5,16 @@ import (
 	"time"
 )
 
+// StripPrefix uses the next layer of the middleware stack as the http.Handler
+// parameter of http.StripPrefix.
 func StripPrefix(prefix string) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.StripPrefix(prefix, next)
 	}
 }
 
+// Timeout uses the next layer of the middleware stack as the http.Handler
+// parameter of http.TimeoutHandler.
 func Timeout(dt time.Duration, msg string) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.TimeoutHandler(next, dt, msg)
